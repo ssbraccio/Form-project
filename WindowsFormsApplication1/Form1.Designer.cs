5bc2b988dea5a7c9d4376c9d90b35fd7c283c167
Sully Braccio <ssbracc@gmail.com>
2015-07-29 10:43:23 -0400

    this is my first commit on project probate

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbOne = new System.Windows.Forms.ListBox();
+            this.sMFName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbOne
+            // 
+            this.lbOne.FormattingEnabled = true;
+            this.lbOne.Location = new System.Drawing.Point(166, 29);
+            this.lbOne.Name = "lbOne";
+            this.lbOne.Size = new System.Drawing.Size(303, 160);
+            this.lbOne.TabIndex = 0;
+            // 
+            // sMFName
+            // 
+            this.sMFName.Location = new System.Drawing.Point(35, 45);
+            this.sMFName.Name = "sMFName";
+            this.sMFName.Size = new System.Drawing.Size(100, 20);
+            this.sMFName.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Insert Images";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "First Film and Roll";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 617);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sMFName);
+            this.Controls.Add(this.lbOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbOne;
+        private System.Windows.Forms.TextBox sMFName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+
+
+    }
+    
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string ImageDir = desktopPath + "\\IMAGE_PATH\\"; 
+
+            DirectoryInfo di = new DirectoryInfo(ImageDir);
+            if (di.Exists)
+            {
+                System.IO.DirectoryInfo[] dirInfos = di.GetDirectories();
+                
+                foreach (System.IO.DirectoryInfo d in dirInfos)
+                {
+                    lbOne.DataSource = dirInfos;
+                }
+
+                for (int i = 0; i < this.lbOne.Items.Count; i++)
+                {
+                    this.lbOne.SetSelected(i, true);
+
+                    String item = lbOne.SelectedItem.ToString();
+
+    
+                    string destfilename =   item + "-0000.tif";
+      
+          
+
+  
+                    DirectoryInfo d = new DirectoryInfo(desktopPath);
+
+                    string sourceFile = desktopPath + "\\IMAGE_FILE\\" + sMFName.Text + ".TIF";
+                    string destFile = desktopPath +  "\\IMAGE_PATH\\" + item + "\\"  + destfilename;           
+               
+
+                    Directory.SetCurrentDirectory(ImageDir + item);
+
+                    // sMFName = 413-044 entered by user 
+
+                    string sMF = "";
+                    String sLMF = "";
+                    Boolean bIncrement = true;
+
+
+
+                    if (File.Exists(item + @"-0000.tif"))
+                    {
+                        System.Diagnostics.Debug.WriteLine("source" + sourceFile);
+
+                    }
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("Dest" + destFile);
+
+                        // convert to number - increment and convert back to string 
+                        if (String.IsNullOrEmpty(sMFName.Text))
+                        {
+                            MessageBox.Show("Please enter the next file name", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                           
+                        }
+                        else
+                        {
+                            if (String.IsNullOrEmpty(sMFName.Text))
+                            {
+                                sMFName.Text = "000-0000";
+                            }
+
+
+                            sourceFile = Path.GetFullPath(sourceFile);
+                            destFile = Path.GetFullPath(destFile) ;
+                            if (System.IO.File.Exists(destFile))
+                            {
+                                bIncrement = false;
+                            }
+                            else
+                            {
+                                File.Move(sourceFile, destFile);
+                                bIncrement = true;
+
+                            }
+                        }
+                    }
+                    if (bIncrement == true)
+                    {
+                        if (sMFName.Text.Length >= 8)
+                        {
+                            sMF = sMFName.Text.Substring(4, 4);
+                            sLMF = sMFName.Text.Substring(0, 3);
+                        }
+                        // sMF = 004                 
+                        // nMF = 5
+                        Int32 nMF = Convert.ToInt32(sMF) + 1;
+                        // sMF = 0005
+                        sMF = "000" + Convert.ToString(nMF);
+                        if (sMF.Length > 4)
+                        {
+                            sMF = sMF.Substring(sMF.Length-4);
+    //                            sMF = sMF.Remove(0, 1);
+                         }
+                           
+                         
+                        // 
+                        sMFName.Text = sLMF + @"-" + sMF;
+                    }
+                }
+            }
+        }
+
+         
+
+    }
+
+
+}
+        
+        
+
+    
+
+
